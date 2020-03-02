732a423ebb3173a89bf1269dd123497021a7b239
Antonio J Pérez-Luque <ajpelu@gmail.com>
2020-03-02 13:36:09 +0100

    update pkg

--- R/createLandscape.R
+++ R/createLandscape.R
@@ -2,26 +2,10 @@
 #'
 #' Create a virtual landscape with different patches.
 #'
-#' This function creates a virtual landscape composed by:
-#' * a target pine plantation
-#' * natural forests
-#' * crops
-#' * shrublands
-#'
-#' The size of the pine plantation and natural forest patchs are specified
-#' by @param size_pp and @param size_nf.
-#'
-#' First the pine plantation patch is set with the dimensions specified by
-#' @param size_pp. Then a number of natural forest patchs (@param n_nf,
-#' between 1 and 5) are created with a determined dimensions (@param size_nf).
-#' The crop patches (a random number between 3 and 8) are then established
-#' according to the remaining space. Finally, remaing pixels are assigned to
-#' the shrubland category.
-#'
 #' @param r A \code{raster} object
 #'
-#' @param size_pp The size of pine plantations expressed as number of
-#' pixels (\code{integer})
+#' @param size_pp The size of pine plantation expressed as pixel numbers
+#' (\code{integer})
 #'
 #' @param n_nf Number of natural forest patchs to create
 #' (\code{integer}, between 1 and 5)
@@ -30,16 +14,39 @@
 #' between 10 and 500) expressed as number of pixels
 #'
 #' @return A \code{raster} object with the landscape configured
-#' @examples
-#' m <- matrix(nrow=100, ncol=200, byrow = TRUE)
-#' r <- raster(m)
-#' extent(r) <- matrix(c(0, 0, 200, 100), nrow=2)
-#' r[] <- 0
-#' myl <- createLandscape(r, size_pp = 1000, size_nf = 500, n_nf = 4)
-#' plot(myl)
+#'
+#' @details
+#' \itemize{This function creates a virtual landscape composed by:
+#'   \item a target pine plantation
+#'   \item natural forests
+#'   \item crops
+#'   \item shrublands
+#'}
+#' The size of the pine plantation and natural forest patchs are specified
+#' by \code{size_pp} and \code{size_nf}.
+#' First the pine plantation patch is set with the dimensions specified by
+#' \code{size_pp}. Then a number of natural forest patchs (\code{n_nf},
+#' between 1 and 5) are created with a determined size (\code{size_nf}).
+#' The crop patches (a random number between 3 and 8) are then established
+#' according to the remaining space. Finally, remain pixels are assigned to
+#' the shrubland category. The size and proportion of the each patch classes
+#' range according to distribution of those ecosystem in Sierra Nevada (SE
+#' Spain) (see Pérez-Luque et al. 2014; 2019)
+#'
+#' @references
+#'
+#' Pérez-Luque AJ, Bonet FJ, Pérez-Pérez R, Aspizua R, Lorite J, Zamora R
+#' (2014). Sinfonevada: Dataset of floristic diversity in Sierra Nevada forests
+#' (SE Spain). PhytoKeys 35: 1 – 15.
+#' \href{https://doi.org/10.3897/phytokeys.35.6363}{10.3897/phytokeys.35.6363}
+#'
+#' Pérez-Luque AJ, Bonet FJ, Zamora R (2019). Map of ecosystems types in Sierra
+#' Nevada mountain (southern Spain).
+#' \url{https://doi.pangaea.de/10.1594/PANGAEA.910176}
+#'
 #' @import raster
 #' @import landscapeR
-#' @author Antonio J Pérez-Luque
+#' @author Antonio J Pérez-Luque (\email{ajpelu@@gmail.com})
 
 
 createLandscape <- function(r, size_pp, n_nf, size_nf) {
