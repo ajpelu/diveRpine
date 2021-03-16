e7608ab3e900e08c7735d5cbdb1f3bbe97ef7d88
Antonio J Perez-Luque <ajpelu@gmail.com>
2021-03-16 12:05:22 +0100

    update params

--- inst/app/init_param.R
+++ inst/app/init_param.R
@@ -1,53 +1,50 @@
-### -------------------------------
-# Initial configuration
+# Load pkgs
+library(raster)
 
-# Height for plotOutput
-h_plots <- 700
-
-## Create empty landscape
+## -------------------------------------------------
+### Created an empty landscape
 set.seed(123)
-ancho <- 63 * 2
-alto <- 53 * 2
+
+# size
+ancho <- 63 * 2 # n cols
+alto <- 53 * 2 # n rows
 
 m <- matrix(nrow=alto, ncol=ancho, byrow = T)
-r <- raster(m)
-extent(r) <- matrix(c(0, 0, ancho, alto), nrow=2)
-r[] <- 0
+empty_landscape <- raster(m)
+# Define extent of raster
+extent(empty_landscape) <- matrix(c(0, 0, ancho, alto), nrow=2)
+empty_landscape[] <- 0
+## -------------------------------------------------
 
+## -------------------------------------------------
+### Position for target_plantation
+position_pine <- matrix(c(nrow(empty_landscape) / 2,
+                          ncol(empty_landscape) / 2),
+                        ncol = 2, nrow = 1)
+## ------------------------------------------------
+
+## -------------------------------------------------
+### Potential number of crops patches
+n_crops <- sample(3:8, size = 1)
+## -------------------------------------------------
+
+## -------------------------------------------------
 ## Some parameters
 line_pol <- 2 ### Line width polygon
 pp_value <- 1 ### Value for Pine plantation
 nf_value <- 2 ### Value for Natural forest
+h_plots <- 700 ### Set height plots (for dashboard)
+## -------------------------------------------------
 
-# Richness range (see package references)
+## -------------------------------------------------
+# Richness range for each land-use (see package references)
 ri_range <- as.data.frame(
   cbind(value = c(0,1,2,3),
         lowRich = c(0, 12.82, mean(13.72, 15.62), 1),
         upRich = c(0, 13.34, mean(16.11, 19.66), 2)))
+## -------------------------------------------------
 
-# Input year/m2 (see package references)
-piBird = (3.7)/50
-piMammal = (0.2)/50
-
-# Themes for raster richness
-richness_theme <- rasterTheme(region = brewer.pal(9, "YlGn"),
-                              axis.line = list(col = "transparent"),
-                              layout.heights = list(xlab.key.padding= 12))
-
-colorkey_richness <- list(space = "bottom",
-                          title = expression(
-                            bold(paste("N",~degree, " species", sep=""))),
-                          title.gpar = list(cex = 1.2))
-
-propagule_theme <- rasterTheme(region = brewer.pal(9, "RdBu"),
-                               axis.line = list(col = "transparent"),
-                               layout.heights = list(xlab.key.padding= 12))
-
-# Spinners Customization
-# see https://cran.r-project.org/web/packages/shinycssloaders/shinycssloaders.pdf
-
-spinnerType <- 8
-spinnerSize <- 0.7
-
-
-
+## ---------------------------------------------------
+# Propagule input density (numbers of propagule / m2 year) (see package references)
+piBird = (3.7)/100
+piMammal = (0.2)/100
